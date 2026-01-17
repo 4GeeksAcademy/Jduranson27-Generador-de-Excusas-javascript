ff1d76f1908a6543ae15e009ac6f61fe89ce1689
Jean Duranson <152572363+jduranson27@users.noreply.github.com>
2026-01-17 00:37:23 +0000

    generador de excusas realizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+   function generateExcuse (array) {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index]
+  }
+
+  let tagExcuse = document.querySelector("#excuse");
+  tagExcuse.innerHTML = generateExcuse(who) +" "+ generateExcuse(action) +" "+ generateExcuse(what) +" "+ generateExcuse(when);
+ 
 };
